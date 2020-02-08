68be22d28f419b6bd60fd4fd00dd5cc7020c9cc1
Anson LowZF <zhanfaye@gmail.com>
2020-02-08 09:10:57 +0800

    Add question to faq section

--- src/question-answer.js
+++ src/question-answer.js
@@ -7,4 +7,8 @@ export const questionAnswers = [
     question: `What is the thickness?`,
     answer: `Quartz stone - 15mm & 20 mm, granite - 15mm, marble 13mm & 15mm`,
   },
+  {
+    question: `The standard of quartz stone`,
+    answer: `International SGS Standard`,
+  },
 ]
